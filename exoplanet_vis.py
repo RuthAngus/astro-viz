8cad25645f559faeb2e8b9d06364301740872b83
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-02-06 19:02:09 -0500

    adding code and data

---
+++ exoplanet_vis.py
@@ -0,0 +1,94 @@
+# Make some plots of exoplanet populations.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
+plotpar = {'axes.labelsize': 18,
+           'font.size': 10,
+           'legend.fontsize': 18,
+           'xtick.labelsize': 18,
+           'ytick.labelsize': 18,
+           'text.usetex': True}
+plt.rcParams.update(plotpar)
+
+
+def period_radius(df):
+    plt.clf()
+    plt.plot(np.log10(df.pl_orbper), np.log10(df.pl_radj*radius_ratio), "k.",
+            alpha=.5)
+    plt.xlabel("$\log_{10}(\mathrm{Orbital~Period~[days]})$")
+    plt.ylabel("$\mathrm{Planet~Radius~}[R_{\mathrm{Earth}}]$")
+    # plt.xlim(0, 10)
+    plt.subplots_adjust(left=.16, bottom=.15)
+    plt.savefig("period_vs_radius")
+
+
+def ra_dec(df):
+    """
+    Transform to xyz.
+    """
+    plt.rcParams['axes.facecolor'] = 'black'
+
+    # Select only transiting planets.
+    m = df.pl_kepflag.values == 1
+    kepler = df.iloc[m]
+
+    ra, dec = kepler.ra.values, kepler.dec.values
+    c_icrs = SkyCoord(ra=ra*u.degree, dec=dec*u.degree, frame='icrs')
+    l = c_icrs.galactic.l.deg
+    b = c_icrs.galactic.b.deg
+
+    plt.clf()
+    plt.figure(figsize=(20,20))
+    plt.plot(l, b, "w.", ms=10)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.savefig("l_vs_b")
+
+
+def orbiting_planet():
+    host_x, host_y = 0, 0
+
+    a, period, slow_factor = 10, 10, 5
+    random_phase = np.random.uniform(0, 2*np.pi)
+    inc = 91
+
+    nframes = 100
+    x = a * np.cos(2*np.pi/(period*slow_factor) * np.arange(nframes)
+                   + random_phase)
+    z = a * np.sin(2*np.pi/(period*slow_factor) * np.arange(nframes)
+                   + random_phase) * np.sin(inc)
+    y = z
+    # y = np.zeros_like(x)
+
+    for i in range(nframes):
+        plt.clf()
+        if z[i] > 1:
+            zdr_star, zdr_planet = 0, 1
+        else:
+            zdr_star, zdr_planet = 1, 0
+        plt.plot(host_x, host_y, ".", color="yellow", ms=100, zorder=zdr_star)
+        plt.plot(x[i], y[i], "k.", zorder=zdr_planet)
+        plt.xlim(-10, 10)
+        plt.ylim(-10, 10)
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.savefig("orbit_movie/frame_{}".format(str(i).zfill(4)))
+
+    # Make the movie file
+    import os
+    # framerate = 30
+    framerate = 20
+    quality = 25
+    os.system("/Applications/ffmpeg -r {0} -f image2 -s 1920x1080 -i "\
+            "orbit_movie/frame_%04d.png -vcodec libx264 -crf {1}  -pix_fmt "\
+            "yuv420p orbit_movie.mp4".format(framerate, quality))
+
+
+if __name__ == "__main__":
+    radius_ratio = 6.991e7/6.371e6
+    df = pd.read_csv("planets.csv", skiprows=69)
+    ra_dec(df)
+
+    # orbiting_planet()
---
+++ vis_movie.py
@@ -0,0 +1,80 @@
+# Make frames of exoplanet orbits.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+plotpar = {'axes.labelsize': 18,
+           'font.size': 10,
+           'legend.fontsize': 18,
+           'xtick.labelsize': 18,
+           'ytick.labelsize': 18,
+           'text.usetex': True}
+plt.rcParams.update(plotpar)
+plt.rcParams['axes.facecolor'] = 'black'
+
+
+def get_orbit(a, radius, period, frames, xcenter=0, ycenter=0,
+              slow_factor=30):
+    """
+    nframes = snapshots per orbit.
+    if period long snapshots per orbit goes up.
+    if period short, snapshots per orbit goes down.
+    snapshots per orbit (nframes) is proportional to the period and
+    1/angular frequency.
+    """
+    random_phase = np.random.uniform(0, 2*np.pi)
+    x = a * np.cos(np.pi/(period*slow_factor) * np.arange(nframes)
+                   + random_phase)
+    y = a * np.sin(np.pi/(period*slow_factor) * np.arange(nframes)
+                   + random_phase)
+    return x, y
+
+if __name__ == "__main__":
+
+    # Read in the data
+    df = pd.read_csv("planets.csv", skiprows=69)
+
+    # Select only transiting planets.
+    m = []
+    for i, method in enumerate(list(df.pl_discmethod.values)):
+        if method == "Transit":
+            m.append(i)
+    df = df.iloc[np.array(m)]
+
+    radius_ratio = 6.991e7/6.371e6  # Convert to Earth radii
+    nframes = 800
+    assert nframes % 2 == 0, "nframes must be an even number"
+    nstar = 800
+
+    a_s = df.pl_orbsmax.values
+    radii = df.pl_radj.values * radius_ratio
+    periods = df.pl_orbper.values
+    xpositions = np.zeros((nstar, nframes))
+    ypositions = np.zeros((nstar, nframes))
+    for star in range(nstar):
+        x, y = get_orbit(a_s[star], radii[star], periods[star], nframes)
+        xpositions[star, :] = x
+        ypositions[star, :] = y
+
+    lim_min = -np.logspace(-2, np.log10(.5), nframes)
+    lim_max = np.logspace(-2, np.log10(.5), nframes)
+    for pos in range(nframes):
+        plt.clf()
+        for star in range(nstar):
+            plt.scatter(xpositions[star, pos], ypositions[star, pos],
+                        s=radii[star]*1./(lim_max[pos]-lim_min[pos]),
+                        c="w", alpha=.5)
+        plt.xlim(lim_min[pos], lim_max[pos])
+        plt.ylim(lim_min[pos], lim_max[pos])
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.savefig("movie/frame_{}".format(str(pos).zfill(4)))
+
+    # Make the movie file
+    import os
+    # framerate = 30
+    framerate = 20
+    quality = 25
+    os.system("/Applications/ffmpeg -r {0} -f image2 -s 1920x1080 -i "\
+            "movie/frame_%04d.png -vcodec libx264 -crf {1}  -pix_fmt "\
+            "yuv420p test.mp4".format(framerate, quality))
