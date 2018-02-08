d7e021500fe2c090233e335151ea3e5d5e2e594e
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-02-08 11:57:01 -0500

    choosing a single star

--- plot_single_light_curve.py
+++ plot_single_light_curve.py
@@ -27,14 +27,17 @@ filenames = ["/Users/ruthangus/.kplr/data/lightcurves/{0}"
                 .format(str(kepids.kepid.values[i]).zfill(9))
                 for i in range(nstars)]
 
-for i, filename in enumerate(filenames[:10]):
-    x, y, yerr = kd.load_kepler_data(filename)
-    plt.clf()
-    plt.figure(figsize=(20, 10))
-    l, h = 2000, 5000
-    x, y = x[l:h]-x[l:h][0], y[l:h]
-    plt.plot(x, y*1e6, "k.", ms=5)
-    plt.xlabel("$\mathrm{Time~[Days]}$")
-    plt.ylabel("$\mathrm{Flux~[parts~per~million]}$")
-    plt.savefig("lightcurves/{}_lc".format(kepids.kepid.values[i]))
-    plt.close()
+# for i, filename in enumerate(filenames[:10]):
+index = 8
+filename = filenames[index]
+x, y, yerr = kd.load_kepler_data(filename)
+plt.clf()
+plt.figure(figsize=(20, 10))
+l, h = 2000, 5000
+x, y = x[l:h]-x[l:h][0], y[l:h]
+plt.plot(x, y*1e6, "k.", ms=5)
+plt.xlabel("$\mathrm{Time~[Days]}$")
+plt.ylabel("$\mathrm{Flux~[parts~per~million]}$")
+plt.xlim(0, max(x))
+plt.savefig("lightcurves/{}_lc".format(kepids.kepid.values[index]))
+plt.close()
