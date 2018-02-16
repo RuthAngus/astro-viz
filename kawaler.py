9367f07186f61c8dbcf22185b190704dba855a98
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-02-16 13:37:22 -0800

    adding kawaler.py

--- kawaler.py
+++ kawaler.py
@@ -39,32 +39,45 @@ def calc_periods(omega_init, R, M, delta_t, niter=100):
     return os, ps
 
 
+def J_conserv(R, M):
+    return R**2 * M
+
+
 if __name__ == "__main__":
-
-    # periods = np.linspace(0, 20)
-    # omegas = 2 * np.pi / periods
-    # plt.plot(omegas, dOdt(omegas, 1, 1), ".")
-    # plt.xlabel("omega")
-    # plt.ylabel("dJ/dt")
-    # plt.savefig("test")
-    # plt.close()
-    # assert 0
-
-    R = np.array([1, 1, 1])
-    M = np.array([1, 1, 1])
-    period_init = np.array([2, 4, 4.5])
-    omega_init = 2 * np.pi / period_init
-    delta_t = 1e-2
 
     plt.figure(figsize=(16, 9))
     niter = 100
-    for i in range(3):
-        os, ps = calc_periods(omega_init[i], R[i], M[i], delta_t, niter=niter)
-        # plt.plot(np.arange(niter) * delta_t * 1000, ps-.2*(period_init[i]))
-        plt.plot(np.arange(niter) * delta_t * 500, ps)
 
-    plt.xlabel("$\mathrm{Time~[Myr]}$")
+    R = np.array([1, 1, 1])
+    M = np.array([1, 1, 1])
+    period_init = np.array([3, 4, 4.5])
+    omega_init = 2 * np.pi / period_init
+    delta_t = 1e-2
+
+    rad_init = np.array([2, 2, 2])
+    nrad = 20
+
+    periods = np.zeros((3, nrad + niter))
+    # For three stars:
+    for i in range(3):
+
+        # Contraction onto the main sequence.
+        radii = np.linspace(rad_init[i], R[i], nrad)
+        masses = np.ones_like(radii) * M[i]
+        p_contract = J_conserv(radii, masses)
+        correction = period_init[i] - p_contract[-1]
+        p_contract += correction
+        periods[i, :nrad] = p_contract
+
+        # Kawaler spin down.
+        os, ps = calc_periods(omega_init[i], R[i], M[i], delta_t, niter=niter)
+        periods[i, nrad:] = ps
+
+        plt.plot(np.arange(np.shape(periods)[1]) * delta_t * 500 - 100,
+                 periods[i])
+
+    plt.xlabel("$\mathrm{Time~Since~ZAMS~[Myr]}$")
     plt.ylabel("$P_{\mathrm{rot}}~\mathrm{[Days]}$")
     # plt.xlim(0, 500)
-    plt.savefig("test")
+    plt.savefig("kawaler")
     plt.close()
